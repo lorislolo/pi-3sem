f3f08ce962f7cb33864943370d7d0f752b236816
lorislolo <lorena.macedo@aluno.ifsp.edu.br>
2024-05-27 17:38:03 -0300

    api de cidades e estados implementado na seção de "Estado" do forms

--- toten/src/pages/Estado.js
+++ toten/src/pages/Estado.js
@@ -1,26 +1,72 @@
 import Button from '../components/Button'
 import styles from './Estado.module.css'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-function Estado () {
-    return(
+function Estado() {
+    const [estados, setEstados] = useState([]);
+    const [cidades, setCidades] = useState([]);
+    const [estadoSelecionado, setEstadoSelecionado] = useState('');
+    const [cidadeSelecionada, setCidadeSelecionada] = useState('');
+
+    useEffect(() => {
+        axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .then(response => {
+                const estados = response.data;
+                setEstados(estados);
+            })
+            .catch(error => {
+                console.error('Erro ao buscar os estados:', error);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (estadoSelecionado) {
+            axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoSelecionado}/municipios`)
+                .then(response => {
+                    const cidades = response.data;
+                    setCidades(cidades);
+                })
+                .catch(error => {
+                    console.error('Erro ao buscar as cidades:', error);
+                });
+        }
+    }, [estadoSelecionado]);
+    return (
         <div className={styles.container}>
             <label htmlFor='Estado'>Escolha seu Estado</label>
-            <select name="Estado" id="Estado">
-            <option value="">Estado...</option>
-                <option value="Minas Gerais">Minas Gerais</option>
-                <option value="São Paulo">São Paulo</option>
+            <select
+                id="estado"
+                value={estadoSelecionado}
+                onChange={e => setEstadoSelecionado(e.target.value)}
+                name="Estado">
+                <option value="">Estado...</option>
+                {estados.map(estado => (
+                    <option key={estado.id} value={estado.sigla}>
+                        {estado.nome}
+                    </option>
+                ))}
             </select>
 
             <label htmlFor='Cidade'>Esolha sua Cidade</label>
-            <select name="Cidade" id="Cidade">
-            <option value="">Cidade...</option>
-                <option value="São Paulo">São Paulo</option>
-                <option value="Caraguatatuba">São José Dos Campos</option>
+            <select
+                name="Cidade"
+                id="cidade"
+                value={cidadeSelecionada}
+                onChange={e => setCidadeSelecionada(e.target.value)}
+                disabled={!estadoSelecionado}
+            >
+                <option value="">Cidade...</option>
+                {cidades.map(cidade => (
+                    <option key={cidade.id} value={cidade.nome}>
+                        {cidade.nome}
+                    </option>
+                ))}
             </select>
 
             <div className={styles.buttonPosition}>
-                <Button url='/Cidade' type="button" value="Voltar"/>
-                <Button url='/NomeIdade' type="button" value="Avançar"/>
+                <Button url='/Cidade' type="button" value="Voltar" />
+                <Button url='/NomeIdade' type="button" value="Avançar" />
             </div>
         </div>
     )
