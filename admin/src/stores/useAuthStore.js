b53c90a40e5cfaeee83e6632cef33ac4036705c5
Clemerson Santos <clemerson.santos@aluno.ifsp.edu.br>
2024-06-08 12:38:47 -0300

    table funcionarios terminada

--- admin/src/stores/useAuthStore.js
+++ admin/src/stores/useAuthStore.js
@@ -1,12 +1,8 @@
 import { create } from 'zustand'
-import { useNavigate } from 'react-router-dom'
 
 const useAuthStore = create((set) => ({
   token: null,
-  login: async (token) => {
-    set(() => ({ token }))
-    navigate('/dashboard')
-  },
+  login: (token) => set(() => ({ token })),
   logout: () => set(() => ({ token: null })),
 }))
 
