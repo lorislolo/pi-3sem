8760bf96c2693e999d262b904dc3fa9960216e2d
leonardo <leonardolfelix12@gmail.com>
2024-06-17 13:53:18 -0300

    feat: Add redirect after successful registration

--- src/totem/pages/CadastrarOutro.js
+++ src/totem/pages/CadastrarOutro.js
@@ -1,14 +1,28 @@
+import { useEffect } from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 import Button from '../components/Button'
 import ButtonImage from '../components/ButtonImage'
 import H1 from '../components/H1'
 import styles from './CadastrarOutro.module.css'
 
 function CadastrarOutro() {
+  const nav = useNavigate()
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      nav('/')
+    }, 2000)
+
+    return () => clearTimeout(timeout)
+  }, [history])
+
   return (
     <div className={styles.container}>
-      <H1 text="Pronto!Cadastrado" />
+      <H1 text="Pronto! Cadastrado" />
       <div className={styles.buttonPosition}>
-        <Button type="button" value="Finalizar!" url="/" />
+        <Link to="/">
+          <Button type="button" value="Finalizar!" />
+        </Link>
       </div>
     </div>
   )
